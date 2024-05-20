814f635082f26fe1691122e89d12e5fc0b546748
tayebhatem <tayebhatem98@gmail.com>
2024-05-20 15:45:41 +0200

    new commit

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function TransfromationsPage() {
+const TransfromationsPage = () => {
   return (
     <div>TransfromationsPage</div>
   )
 }
+
+export default TransfromationsPage
--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-export const UpdateTransformations = () => {
+ const UpdateTransformations = () => {
   return (
     <div>UpdateTransformations</div>
   )
 }
+export default UpdateTransformations
